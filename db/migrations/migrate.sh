91aca1bcda1641084bd392f684a88baadb9f0038
aPruner <adampruner99@gmail.com>
2020-05-25 16:39:01 -0700

    add a simple migration with goose to create the users table, and add a migrate script that builds a conn string from the root .env and passes it to goose to do the migrations

---
+++ db/migrations/migrate.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+env_vars=$(egrep -v '^#' ../../.env | xargs)
+for env_var in $env_vars
+do
+  var_key="${env_var%=*}"
+  var_val="${env_var#*=}"
+  if [ "$var_key" = "DB_USER" ]
+  then
+    user_val=$var_val
+  elif [ "$var_key" = "DB_PASSWORD" ]
+  then
+    password_val=$var_val
+  elif [ "$var_key" == "DB_NAME" ]
+  then
+    dbname_val=$var_val
+  fi
+done
+
+conn_string="user=${user_val} password=${password_val} dbname=${dbname_val} sslmode=disable"
+
+if [ "$1" = "up" ]
+then
+  goose postgres "${conn_string}" up
+elif [ "$1" = "down" ]
+then
+  goose postgres "${conn_string}" down
+else
+  echo "Please provide an up or down argument to the migrate script, like so:"
+  echo
+  echo "  migrate up"
+  echo
+  echo "or"
+  echo
+  echo "  migrate down"
+fi
+
+
