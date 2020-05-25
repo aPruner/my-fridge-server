d4635e2c3fd9ea736492ee485e54f5f6e02b6577
aPruner <adampruner99@gmail.com>
2020-05-25 12:51:05 -0700

    add a basic db query to get users by username, need to hook it up to the gql resolver now

--- db/db.go
+++ db/db.go
@@ -4,13 +4,14 @@ import (
 	"database/sql"
 	"fmt"
 	_ "github.com/lib/pq"
+	"log"
 )
 
 type Db struct {
 	*sql.DB
 }
 
-func BuildConnString(host string, port int, user string, password string ,dbName string) string {
+func BuildConnString(host string, port int, user string, password string, dbName string) string {
 	return fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
 		host, port, user, password, dbName)
 }
@@ -25,4 +26,30 @@ func Create(connString string) (*Db, error) {
 		return nil, err
 	}
 	return &Db{db}, nil
+}
+
+func (d *Db) GetUsersByUsername(username string) []User {
+	sqlStatement, err := d.Prepare("SELECT * FROM users WHERE username=$1")
+	if err != nil {
+		log.Printf("GetUsersByUsername sql prep error: %v", err)
+	}
+
+	rows, err := sqlStatement.Query(username)
+	if err != nil {
+		log.Printf("GetUsersByUsername sql query error: %v", err)
+	}
+
+	var r User
+	var users []User
+	for rows.Next() {
+		err = rows.Scan(
+			&r.ID,
+			&r.Username,
+		)
+		if err != nil {
+			log.Printf("GetUsersByUsername scanning rows error: %v", err)
+		}
+		users = append(users, r)
+	}
+	return users
 }
---
+++ db/models.go
@@ -0,0 +1,6 @@
+package db
+
+type User struct {
+	ID       int
+	Username string
+}
