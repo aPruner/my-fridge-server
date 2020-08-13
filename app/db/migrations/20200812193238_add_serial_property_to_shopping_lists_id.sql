9a5b3a2cd7fc915e1dfdc4a77713e29d31883ca5
aPruner <adampruner99@gmail.com>
2020-08-12 19:36:48 -0700

    fix shopping_lists ID incremental id sequence name

--- app/db/migrations/20200812193238_add_serial_property_to_shopping_lists_id.sql
+++ app/db/migrations/20200812193238_add_serial_property_to_shopping_lists_id.sql
@@ -1,11 +1,11 @@
 -- +goose Up
 SELECT MAX(id) + 1 FROM shopping_lists;
-CREATE SEQUENCE user_id_sequence START WITH 1;
-ALTER TABLE shopping_lists ALTER COLUMN id SET DEFAULT nextval('user_id_sequence');
+CREATE SEQUENCE shopping_lists_id_sequence START WITH 1;
+ALTER TABLE shopping_lists ALTER COLUMN id SET DEFAULT nextval('shopping_lists_id_sequence');
 ALTER TABLE shopping_lists ALTER COLUMN id SET NOT NULL;
-ALTER SEQUENCE user_id_sequence OWNED BY shopping_lists.id;
+ALTER SEQUENCE shopping_lists_id_sequence OWNED BY shopping_lists.id;
 
 
 -- +goose Down
 ALTER TABLE shopping_lists ALTER COLUMN id DROP DEFAULT;
-DROP SEQUENCE user_id_sequence;
+DROP SEQUENCE shopping_lists_id_sequence;
